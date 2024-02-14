dac7240e26a93ac6f2f5b8c80503f3174c5c880c
Teymur Mammadov <teymur.mammadov@ozu.edu.tr>
2024-02-14 16:02:03 +0300

    Project 0

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\tm023756\\Documents\\GitHub\\Project_0\\src\\test.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st ;
+        String prime;
+        int a;
+        PrintWriter writer = new PrintWriter("output.txt");
+        while ((st = br.readLine()) != null){
+            a= Integer.parseInt(st);
+            if (primeTest(a)==1){
+                prime = String.valueOf(a);
+                writer.write(prime);
+            }
+        }
+        writer.close();
+    }
+    public static int primeTest(int n){
+
+        if(n<=1){
+            return 0;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return 0;
+
+            }
+            }
+        return 1;
+
+    }
+}
